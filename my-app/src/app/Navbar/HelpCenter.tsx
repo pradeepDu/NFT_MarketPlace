c3e9db43fb4d99e36480b735a9cdaa7cb36e9951
pradeepDu <dukemarquis2004@gmail.com>
2024-12-22 12:59:19 +0530

    completed contract and defined directory structure

---
+++ my-app/src/app/Navbar/HelpCenter.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+export default class HelpCenter extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <div>HelpCenter</div>
+    )
+  }
+}
---
+++ my-app/src/app/Navbar/Notification.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+export default class Notification extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <div>Notification</div>
+    )
+  }
+}
